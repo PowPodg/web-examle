26ea8afcfaac2f9d4ce6b4b676af72a6f9f6c2d8
PowPodg <oleg@oleg-iv.tk>
2019-12-07 18:52:17 +0500

    create

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# web-examle
+## Examle Web-Coding   
+
+https://web-example-3e31f.web.app/
---
+++ src/App.js
@@ -0,0 +1,328 @@
+import React from 'react';
+
+import './App.scss';
+
+function App () {
+	return (
+		<div className="App">
+			<div className="header">
+				<div className="headTop">
+					<div className="logo">
+						<img src="imghead/logo.svg" alt="" />
+					</div>
+					<div className="headerText">
+						<span>ГЛАВНАЯ</span>
+						<span>
+							О НАС
+							<img src="imghead/flag.png" alt="" />
+						</span>
+						<span>УСЛУГИ</span>
+						<span>Поддержка предпринимательства</span>
+						<span>Филиалы</span>
+						<span>Новости</span>
+						<span>Контакты</span>
+					</div>
+					<div className="leftHead">
+						<div className="input">
+							<input type="text" />
+							<img src="imghead/search.svg" alt="" />
+						</div>
+					</div>
+					<div className="leftUser">
+						<img src="imghead/user.svg" alt="" />
+					</div>
+				</div>
+				<div className="headMiddle">
+					<div className="middleLeft">
+						<img src="imghead/left.svg" alt="" />
+					</div>
+					<div className="middletext">
+						<span>
+							<p>Xxxx «Xxxxxxx-<br /> xxxxxxxxxxxx xxxxxx</p>
+							<p>Xxxxxxxxxxxx xxxxxxx»</p>
+						</span>
+					</div>
+					<div className="middleRight">
+						<img src="imghead/right.svg" alt="" />
+					</div>
+				</div>
+				<div className="headBottom">
+					<div className="middleblock">
+						<img src="imghead/paginator.svg" alt="" />
+					</div>
+				</div>
+			</div>
+			<div className="header1">
+				<div className="textheader1">
+					<span>В составе палаты на данный момент — более 500 организаций и предпринимателей</span>
+				</div>
+				<div className="blockRect1">
+					<div className="contRect">
+						<div className="rectBusy">
+							<div className="_spancont">
+								<div className="this_head">
+									<span>Вступить</span>
+								</div>
+								<div className="this_text">
+									<span>Cupidatat Lorem sit pariatur quis et sit</span>
+									<span>commodo qui officia cupidatat sint. Voluptate</span>
+									<span>qui dolor fugiat duis fugiat commodo minim</span>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="contRect1">
+						<p>Услуги</p>
+						<div className="rect" />
+					</div>
+				</div>
+				<div className="blockRect1">
+					<div className="contRect1">
+						<p>Комитет и гильдии</p>
+						<div className="rect" />
+					</div>
+					<div className="contRect1">
+						<p>Филиалы</p>
+						<div className="rect" />
+					</div>
+				</div>
+				<div className="imgToTop">
+					<img src="imghead/toTop.svg" alt="" />
+				</div>
+			</div>
+			<div className="header2">
+				<div className="head2_1">
+					<div className="h1-h6">
+						<img src="imghead/h1-h6.svg" alt="" />
+						<span>Новости</span>
+					</div>
+					<div className="group1">
+						<span>Все новости</span>
+						<img src="imghead/grp1.svg" alt="" />
+					</div>
+					<div className="h1-h6">
+						<img src="imghead/h1-h6.svg" alt="" />
+						<span>Анонсы</span>
+					</div>
+					<div className="group1">
+						<span>Все анонсы</span>
+						<img src="imghead/grp1.svg" alt="" />
+					</div>
+				</div>
+				<div className="head2_2">
+					<div className="staticPict">
+						<div className="line">
+							<div className="mini-static">
+								<div className="pictleft" />
+								<div className="pictright">
+									<div className="textNews">
+										<p>«Меновой двор 2019». Итоги</p>
+										<p>12 августа 2019</p>
+									</div>
+								</div>
+							</div>
+							<div className="mini-static">
+								<div className="pictleft" />
+								<div className="pictright">
+									<div className="textNews">
+										<p>ТПП интересует мнение предпринимателей о четырёхдневной рабочей неделе</p>
+										<p>12 августа 2019</p>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div className="line">
+							<div className="mini-static">
+								<div className="pictleft" />
+								<div className="pictright">
+									<div className="textNews">
+										<p>Приказы, которые затронут владельцев зданий и сооружений</p>
+										<p>12 августа 2019</p>
+									</div>
+								</div>
+							</div>
+							<div className="mini-static">
+								<div className="pictleft" />
+								<div className="pictright">
+									<div className="textNews">
+										<p>«Меновой двор 2019». Итоги</p>
+										<p>12 августа 2019</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="staticText">
+						<div className="newsUnit">
+							<p>18.09.2019</p>
+							<p>Обсуждение Стратегии развития жилищно-коммунального хозяйства в РФ до 2035 года</p>
+						</div>
+						<div className="newsUnit">
+							<p>23.09.2019</p>
+							<p>Заседание пресс-клуба ТПП РФ в Сочи</p>
+						</div>
+						<div className="newsUnit">
+							<p>24.09.2019</p>
+							<p>
+								Круглый стол «Профилактика роста производства и оборота нелегальной подакцизной
+								продукции»
+							</p>
+						</div>
+						<div className="newsUnit">
+							<p>24.09.2019</p>
+							<p>Онлайн-трансляция XV Налогового форума «Важные изменения 2019-2020»</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="middle1">
+				<div className="blockCentr">
+					<div className="topblCntr">
+						<div className="_h2">
+							<img src="imghead/_h2.svg" alt="" />
+							<span>Проекты</span>
+						</div>
+					</div>
+					<div className="middleblCntr">
+						<div className="mdcLeft">
+							<img src="imghead/vecLeft.svg" alt="" />
+						</div>
+						<div className="mdcMdl">
+							<div className="blockProj">
+								<div className="contprj">
+									<div className="imgblk">
+										<img src="imghead/gazprom.svg" alt="" />
+									</div>
+									<div className="pdiv">
+										<p>Рефинансирование ипотечных кредитов</p>
+									</div>
+								</div>
+							</div>
+							<div className="blockProj">
+								<div className="contprj">
+									<div className="imgblk">
+										<img src="imghead/middlProj.svg" alt="" />
+									</div>
+									<div className="pdiv">
+										<p>Навигатор успеха</p>
+									</div>
+								</div>
+							</div>
+							<div className="blockProj">
+								<div className="contprj">
+									<div className="imgblk">
+										<img src="imghead/rightProj.svg" alt="" />
+									</div>
+									<div className="pdiv">
+										<p>Технологическое присоеди-нение к электрическим сетям «Оренбургэнерго»</p>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div className="mdcRight">
+							<img src="imghead/vecRight.svg" alt="" />
+						</div>
+					</div>
+					<div className="bottomblCntr">
+						<img src="imghead/paginatorMidl.svg" alt="" />
+					</div>
+				</div>
+			</div>
+			<div className="middle2">
+				<div className="mdl2Cont">
+					<img src="imghead/middl2_1.svg" alt="" />
+					<img src="imghead/middl2_2.svg" alt="" />
+					<img src="imghead/middl2_3.svg" alt="" />
+					<img src="imghead/middl2_4.svg" alt="" />
+					<img src="imghead/middl2_5.svg" alt="" />
+				</div>
+			</div>
+			<div className="map">
+				<div className="rectMap1">
+					<div className="rectMap2" />
+				</div>
+			</div>
+			<div className="footer">
+				<div className="ftheadcont">
+					<div className="fthead">
+						<div className="contFthead">
+							<div className="textBlft">
+								<span>»</span>
+								<span>&nbsp;ГЛАВНАЯ</span>
+							</div>
+							<div className="textBlft">
+								<span>»</span>
+								<span>&nbsp;О НАС</span>
+							</div>
+							<div className="textBlft">
+								<span>»</span>
+								<span>&nbsp;Услуги</span>
+							</div>
+							<div className="textBlft">
+								<span>»</span>
+								<span>&nbsp;ПОДДЕРЖКА ПРЕДПРИНИМАТЕЛЬСТВА</span>
+							</div>
+							<div className="textBlft">
+								<span>»</span>
+								<span>&nbsp;ФИЛИАЛЫ</span>
+							</div>
+							<div className="textBlft">
+								<span>»</span>
+								<span>&nbsp;НОВОСТИ</span>
+							</div>
+							<div className="textBlft">
+								<span>»</span>
+								<span>&nbsp;КОНТАКТЫ</span>
+							</div>
+						</div>
+					</div>
+					{/* <div className="addfon" /> */ }
+					<div className="ftmiddle">
+						<div className="ftmgBlcentr">
+							<div className="_left">
+								<span>КОНТАКТЫ</span>
+								<div className="_strUnit">
+									<p>XXXXXXX, Россия, г. Xxxxxxxx, переулок Xxxxxxxx, Z</p>
+								</div>
+								<div className="_strUnit">
+									<p>Телефон:&nbsp;</p>
+									<p>+7 (3532) XX-XX-XX</p>
+								</div>
+								<div className="_strUnit">
+									<p>Факс:&nbsp;</p>
+									<p>+7 (3532) XX-XX-XX</p>
+								</div>
+								<div className="_strUnit">
+									<p>E-mail:&nbsp;</p>
+									<p>cci@orenburg-cci.ru</p>
+								</div>
+							</div>
+							<div className="_right">
+								<span>ДОПОЛНИТЕЛЬНО</span>
+								<div className="_strUnit">
+									<p>»&nbsp;</p>
+									<p>Сведения об образовательной организации</p>
+								</div>
+								<div className="_imgft">
+									<img src="imghead/vk.svg" alt="" />
+									<img src="imghead/inst.svg" alt="" />
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="bottomft">
+						<div className="cntrbttft">
+							<div className="this_left">
+								<span>© 2019 Xxxxxxx-xxxxxxxxxxxx xxxxxx Xxxxxxxxxxxx xxxxxxx</span>
+							</div>
+							<div className="this_right">
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default App;
